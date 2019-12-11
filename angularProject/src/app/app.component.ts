7ca00fac795904f8fe82af1c84789c5c85de8e9f
pallavisonar1070 <pallavisonar1070@gmail.com>
2019-12-11 23:40:22 +0530

    bootstrap added

--- angularProject/src/app/app.component.ts
+++ angularProject/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularProject';
+  public counter = 0;
+
+  public increment() {
+    this.counter += 1;
+  }
 }
